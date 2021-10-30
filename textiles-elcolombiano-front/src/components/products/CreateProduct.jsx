8e053fe4fbf3d515924ad12da80a227dcff50361
MariaGa <mariamorera240514@gmail.com>
2021-10-30 17:44:25 -0500

    Prueba axios

--- textiles-elcolombiano-front/src/components/products/CreateProduct.jsx
+++ textiles-elcolombiano-front/src/components/products/CreateProduct.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, {useState, Fragment} from 'react';
+import Swal from 'sweetalert2';
+import clienteAxios from '../../config/axios';
 
 function FormProducts() {
+
+    //producto = state, guardarProducto = setstate
+    const [producto, guardarProducto] = useState({
+        nombre: '',
+        precio: ''
+    });
+    // archivo = state, guardarArchivo = setState
+    const [archivo, guardarArchivo] = useState('');
+
+    // almacena el nuevo producto en la base de datos.
+    const agregarProducto = async e => {
+        e.preventDefault();
+
+        // crear un formdata
+        const formData = new FormData();
+        formData.append('nombre', producto.nombre);
+        formData.append('precio', producto.precio);
+        formData.append('imagen', archivo);
+
+        // almacenarlo en la BD
+        try {
+            const res = await clienteAxios.post('/productos', formData, {
+                headers: {
+                    'Content-Type' : 'multipart/form-data'
+                }
+            } );
+
+            // Lanzar una alerta
+            if(res.status === 200) {
+                Swal.fire(
+                    'Agregado Correctamente',
+                    res.data.mensaje,
+                    'success'
+                )
+            }
+
+
+        } catch (error) {
+            console.log(error);
+            // lanzar alerta
+            Swal.fire({
+                type:'error',
+                title: 'Hubo un error',
+                text: 'Vuelva a intentarlo'
+            })
+        }
+    }
+
+    // leer los datos del formulario
+    const leerInformacionProducto = e => {
+        guardarProducto({
+            // obtener una copia del state y agregar el nuevo
+            ...producto,
+            [e.target.name] : e.target.value
+        })
+    }
+
+
     return (
+        <Fragment>
         <div>
             <div className="container">
                 <div className="card">
@@ -11,31 +72,32 @@ function FormProducts() {
                             <div className="row">
                                 <div className="col-md-6 mb-3">
                                     <label className="form-label">Nombre producto</label>
-                                    <input type="text" className="form-control" />
+                                    <input type="text" className="form-control" onChange={leerInformacionProducto}/>
                                 </div>
                                 <div className="col-md-6 mb-3">
                                     <label className="form-label">Valor unidad</label>
-                                    <input type="text" className="form-control" />
+                                    <input type="text" className="form-control" onChange={leerInformacionProducto}/>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col-md-12 mb-3">
                                     <label className="form-label">Descripcion producto</label>
-                                    <input type="textarea" className="form-control" />
+                                    <input type="textarea" className="form-control" onChange={leerInformacionProducto}/>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col-md-12 mb-3 form-check">
                                     <input type="checkbox" className="form-check-input" />
-                                    <label className="form-check-label" >Disponible</label>
+                                    <label className="form-check-label" onChange={guardarArchivo}>Disponible</label>
                                 </div>
                             </div>
-                            <button type="submit" className="btn btn-primary">Guardar</button>
+                            <button type="submit" className="btn btn-primary" onChange={agregarProducto}>Guardar</button>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
+        </Fragment>
     )
 }
 
